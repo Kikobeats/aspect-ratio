37c7fc798d923856d280230a12321e6288765bd0
Andrei Vinaga <andrei.vinaga@ownzones.com>
2019-09-10 10:54:25 +0300

    maximum call stack exceeded when no width and height is sent

--- index.js
+++ index.js
@@ -2,6 +2,13 @@
 const { gcd, highestFirst, formatAspectRatio } = require('./utils')
 
 module.exports = (height, width, seperator = ':') => {
+  if (!height) {
+    throw new Error(`Invalid height: ${height}`);
+  }
+
+  if (!width) {
+    throw new Error(`Invalid width: ${width}`);
+  }
   const [h, w] = highestFirst(height, width)
   const divisor = gcd(h, w)
   return formatAspectRatio(h, w, divisor, seperator)
