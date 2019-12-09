ca445796b771e1fba1f9c2324c844edd239499a5
Kiko Beats <josefrancisco.verdu@gmail.com>
2019-12-09 12:02:04 +0100

    test: ensure to handle invalid input
    closes #13

--- src/index.js
+++ src/index.js
@@ -3,8 +3,18 @@
 const { gcd, highestFirst, formatAspectRatio } = require('./util')
 
 module.exports = (height, width, seperator = ':') => {
-  if (typeof height !== 'number') throw new Error(`Invalid height: ${height}`)
-  if (typeof width !== 'number') throw new Error(`Invalid width: ${width}`)
+  if (typeof height !== 'number') {
+    throw new Error(
+      `Invalid height: expected a \`number\`, received \`${height}\``
+    )
+  }
+
+  if (typeof width !== 'number') {
+    throw new Error(
+      `Invalid width: expected a \`number\`, received \`${width}\``
+    )
+  }
+
   const [h, w] = highestFirst(height, width)
   const divisor = gcd(h, w)
   return formatAspectRatio(h, w, divisor, seperator)
--- test/index.js
+++ test/index.js
@@ -4,49 +4,27 @@ const aspectRatio = require('..')
 const should = require('should')
 
 describe('aspect ratio', () => {
-  it('1024x768', () => {
-    should(aspectRatio(1024, 768)).equal('4:3')
-  })
+  it('1024x768', () => should(aspectRatio(1024, 768)).equal('4:3'))
+  it('1920x1080', () => should(aspectRatio(1920, 1080)).equal('16:9'))
+  it('1280x1024', () => should(aspectRatio(1280, 1024)).equal('5:4'))
+  it('specifying output separator', () =>
+    should(aspectRatio(1920, 1080, '/')).equal('16/9'))
 
-  it('1920x1080', () => {
-    should(aspectRatio(1920, 1080)).equal('16:9')
-  })
-
-  it('1280x1024', () => {
-    should(aspectRatio(1280, 1024)).equal('5:4')
-  })
-
-  it('using other separator', () => {
-    should(aspectRatio(1920, 1080, '/')).equal('16/9')
-  })
-
-  it('invalid width', () => {
-    let error = ''
-    try {
-      should(aspectRatio(1920, null, '/')).equal('16/9')
-    } catch (e) {
-      error = e.message
-    }
-    should(error).equal('Invalid width: null')
-  })
-
-  it('invalid height', () => {
-    let error = ''
-    try {
-      should(aspectRatio(null, 1080, '/')).equal('16/9')
-    } catch (e) {
-      error = e.message
-    }
-    should(error).equal('Invalid height: null')
-  })
-
-  it('invalid width & height', () => {
-    let error = ''
-    try {
-      should(aspectRatio(null, null)).equal('16/9')
-    } catch (e) {
-      error = e.message
-    }
-    should(error).equal('Invalid height: null')
+  describe('invalid input', () => {
+    ;[
+      [1920, null, '/'],
+      [null, 1080, '/'],
+      [null, null],
+      [undefined, undefined],
+      ['1920', '1080']
+    ].forEach(input => {
+      it(JSON.stringify(input), () => {
+        try {
+          aspectRatio(...input)
+        } catch ({ message }) {
+          should(message.includes('Invalid')).be.true()
+        }
+      })
+    })
   })
 })
