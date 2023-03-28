d4b9a41ff21b8f82b6342543d84f520b665a8876
Kiko Beats <josefrancisco.verdu@gmail.com>
2023-03-28 15:04:44 +0200

    refactor: less CLI bytes

--- bin/index.js
+++ bin/index.js
@@ -1,23 +1,36 @@
 #!/usr/bin/env node
 'use strict'
+
+const mri = require('mri')
+
 const ratio = require('..')
-const cli = require('meow')({
-  pkg: '../package.json',
-  help: [
-    'Usage',
-    '  $ aspect-ratio <width> <height> [options]',
-    '\n  options:',
-    "\t -s\t     specified a separator. (by default is ':').",
-    '\t --version   output the current version.',
-    '\n  examples:',
-    '\t aspect-ratio 1920 1080',
-    '\t aspect-ratio 800 600 -s /'
-  ].join('\n')
+
+const help = [
+  'Usage',
+  '  $ aspect-ratio <width> <height> [options]',
+  '\n  options:',
+  "\t -s\t     specified a separator. (by default is ':').",
+  '\t --version   output the current version.',
+  '\n  examples:',
+  '\t aspect-ratio 1920 1080',
+  '\t aspect-ratio 800 600 -s /'
+].join('\n')
+
+const argv = mri(process.argv.slice(2), {
+  alias: {
+    separator: 's'
+  },
+  default: {
+    separator: ':'
+  }
 })
 
-const width = cli.input[0]
-const height = cli.input[1]
-const separator = cli.flags.s || ':'
+const width = Number(argv._[0])
+const height = Number(argv._[1])
 
-if (!height) cli.showHelp()
-else console.log(ratio(width, height, separator))
+if (Number.isNaN(width) || Number.isNaN(height)) {
+  console.log(help)
+  process.exit(1)
+}
+
+console.log(ratio(width, height, argv.separator))
